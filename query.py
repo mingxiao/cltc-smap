b9d60727db1518aae404e0bdfc60e9ad69746178
Ming Xiao <minxiao@ucdavis.edu>
2013-01-17 01:19:47 +0100

    Modify query.py to get range of values from sMAP

--- query.py
+++ query.py
@@ -27,6 +27,44 @@ def get_value(uuid, starttime, unit = 'A'):
     value = data['Readings'][0][1]
     return value
 
+def get_range_value(uuid,starttime,endtime,unit='A'):
+    """
+    Gets a range of values from the timeseries associated with uuid,from starttime to endtime
+
+    OUTPUT
+        values - list of floats
+    INPUT
+        uuid - the uuid of the timeseries, string
+        starttime - start time in UNIX milliseconds since epoch, int
+        endtime - end time in UNIX milliseconds since epoch, int
+        unit - the unit of measure, string
+    """
+    URL = "http://new.openbms.org/backend/api/data/uuid/%s/Properties__UnitofMeasure/%s?starttime=%s&endtime=%s"%(uuid,
+                                                                                                                  unit,
+                                                                                                                  starttime,
+                                                                                                                  endtime)
+    response = urllib2.urlopen(URL)
+    html=response.read()
+    #print html
+    datalist = ast.literal_eval(html)
+    #print len(datalist)
+    datadict = datalist[0]
+    readings = datadict['Readings']
+    #print readings
+    values = [r[1] for r in readings]
+    #print values
+    return values
+
+def unix_ms(month,day,hour,minute,second=0,year=2013):
+    """
+    Returns the milliseconds since UNIX epoch since the date and time inputed
+    """
+    try:
+        some_date = dt.datetime(year, month,day,hour,minute,second)
+        timestamp = float(some_date.strftime('%s.%f'))
+        return int(timestamp*1000)
+    except ValueError:
+        print '! Invalid date or time !'
 
 def epoch_time():
     """
@@ -37,4 +75,7 @@ def epoch_time():
     return int(timestamp*1000)
     
 #print epoch_time()
-print get_value(uuid,epoch_time())
+#print get_value(uuid,epoch_time())
+
+result =get_range_value(uuid,unix_ms(1,15,1,0), unix_ms(1,15,1,30))
+
